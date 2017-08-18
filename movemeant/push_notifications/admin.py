88038eacfbf4dc8c56c29cfcba85178d48d7f197
Adrian Vatchinsky <avatchinsky@outlook.com>
2017-08-18 12:42:08 -0400

    migrations

--- movemeant/push_notifications/admin.py
+++ movemeant/push_notifications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PushNotification
+
+class PushNotificationAdmin(admin.ModelAdmin):
+    model = PushNotification
+    # list_display = ('name', '_total_members', '_total_venues', '_total_reveals', 'time_modified', 'time_created',)
+
+admin.site.register(PushNotification, PushNotificationAdmin)

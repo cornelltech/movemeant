4c8747a18cc01f73d1be27bbea8de37eaf43fdb3
Kostas Livieratos <koslibpro@gmail.com>
2017-08-22 01:31:35 +0300

    fixed device create api view

--- movemeant/push_notifications/views.py
+++ movemeant/push_notifications/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -7,20 +7,6 @@ from .models import Device
 from .serializers import DeviceSerializer
 
 
-class DeviceCreateView(APIView):
-    permission_classes = (IsAuthenticated, )
-
-    def post(self, request, format=None):
-        user = request.user
-        device_token = request.data.get('device', None)
-        
-        if device_token is None:
-            return Response(status=status.HTTP_400_BAD)
-
-        
-        obj, created = Device.objects.get_or_create(
-            user=user,
-            token=device_token
-        )
-
-        return Response(status=status.HTTP_200_OK)
+class DeviceCreateView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = DeviceSerializer

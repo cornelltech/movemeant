1fab25453c7cde728490c4d13c64ab19387fc6c1
Kostas Livieratos <koslibpro@gmail.com>
2017-09-07 18:44:54 +0300

    reverted code for device registration

--- movemeant/push_notifications/views.py
+++ movemeant/push_notifications/views.py
@@ -1,12 +1,24 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Device
-from .serializers import DeviceSerializer
 
 
-class DeviceCreateView(CreateAPIView):
+class DeviceCreateView(APIView):
     permission_classes = (IsAuthenticated,)
-    serializer_class = DeviceSerializer
+
+    def post(self, request, format=None):
+        user = request.user
+        device_token = request.data.get('device', None)
+
+        if device_token is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        obj, created = Device.objects.get_or_create(
+            user=user,
+            token=device_token
+        )
+
+        return Response(status=status.HTTP_200_OK)
